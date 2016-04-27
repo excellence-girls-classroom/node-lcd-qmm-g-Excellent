0aaeb405b3b4744ed823096d2fe477c7e0bf8d39
qmm <qmhao@sina.com>
2016-04-27 16:42:06 +0800

    node -lcd

--- main/toString.js
+++
@@ -1,8 +0,0 @@
-function toString(tempArray){
-    var string = '\n';
-    tempArray.forEach(function(element){
-        string += element + '\n';
-    });
-    return string;
-}
-module.exports = toString;
